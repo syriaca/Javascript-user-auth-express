718aa824d6f6db1c2d3821457a1f7c9844a0dba1
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-09-01 15:53:02 +0200

    Added mongoose to use mongodb database

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
 
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/bookworm', {useNewUrlParser: true})
+var db = mongoose.connection;
+
+//mongo error
+db.on('error', console.error.bind(console, 'connection error:'))
 // parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
