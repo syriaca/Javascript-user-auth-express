097320a2fbcd373c218e11cfe69d2dbb13032299
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-09-03 11:28:06 +0200

    Add middleware function to require login to access some pages

--- middleware/index.js
+++ middleware/index.js
@@ -5,4 +5,15 @@ function loggedOut(req, res, next) {
   return next();
 }
 
+function requireLogin(req, res, next) {
+    if (req.session && req.session.userId) {
+        return next();
+    } else {
+        var err = new Error('You must be logged in to view the page');
+        err.status = 401;
+        return next(err);
+    }   
+}
+
 module.exports.loggedOut = loggedOut;
+module.exports.requireLogin = requireLogin;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var User = require('../models/user');
 var mid = require ('../middleware');
 
 // GET /profile
-router.get('/profile', function(req, res, next){
+router.get('/profile', mid.requireLogin, function(req, res, next){
     if(!req.session.userId) {
       var err = new Error('You are not authorized to view this page.');
       err.status = 403;
